3781248359247506cff45149342b52127358c2a2
unknown <romigit@users.noreply.github.com>
2020-09-17 03:56:46 -0300

    angular material card, toolbar y select. Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import {PicturesService} from './services/pictures.service';
+import {Picture} from './models/picture';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit { //es para que cargue de una las fotografías
   title = 'tp1';
+  pictures: Picture[] = [];
+  albumsId = [1,2,3];
+
+  constructor(public picturesService: PicturesService){}
+
+  ngOnInit(){
+    this.picturesService.getPictures()
+    .subscribe(
+      pictures => {
+                      console.log(pictures),
+                      this.pictures = pictures; //como no reconoce el tipo de dato en this.pictures entonces genero la clase picture. Luego establece que espera pero no sabe que llega
+                  },
+      err      => console.log(err)
+    )
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,19 @@ import { NotFoundComponent } from './form/not-found/not-found.component';
 import { MenuComponent } from './form/menu/menu.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import {MatRippleModule} from '@angular/material/core';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+import {HttpClientModule} from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +35,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    MatTableModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatRippleModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatTooltipModule,
+    MatCardModule,
+   MatToolbarModule,
+
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
